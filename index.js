afdeddc14433c1959ce3412f28e7be2ff03d3de6
Christopher <raphelchris@gmail.com>
2023-12-12 10:18:39 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(recordArray) {
+  const foundRecord = recordArray.find(function(eachRecord) {
+    return eachRecord.result === 'W'
+  })  
+  if (foundRecord === undefined) {
+    return foundRecord;
+  }
+  
+    return foundRecord.year;
+}
+
+
+
+
+
